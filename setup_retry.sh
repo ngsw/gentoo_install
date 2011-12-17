7edbbcce3639863caf69b9078d5b80820c59aab0
ngsw <ngsw@ngsw.jp>
2011-12-17 22:15:02 +0900

    retry after kernel panic

---
+++ setup_retry.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# # dhcpd eth0
+# or
+# # dhcpd -HD eth0
+useradd jipp
+echo 'jipp:jjj
+root:jjj' | chpasswd
+grep root /etc/group|grep jipp || sed -i 's/wheel:x:10:root/wheel:x:10:root,jipp/' /etc/group
+ifconfig eth0 192.168.1.222 broadcast 192.168.1.255 netmask 255.255.255.0 up
+route add default gw 192.168.1.1
+cat > /etc/resolv.conf <<'EOF'
+nameserver 192.168.1.1
+nameserver 8.8.8.8
+EOF
+/etc/init.d/sshd restart
+
+mount /dev/sda3 /mnt/gentoo
+mount /dev/sda1 /mnt/gentoo/boot
+mount -t proc none /mnt/gentoo/proc
+mount --rbind /dev/ /mnt/gentoo/dev
