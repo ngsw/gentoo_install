24be463eec6be1af39ae1a5e162366401deb3f31
ngsw <ngsw@ngsw.jp>
2011-12-18 00:50:16 +0900

    modified:   setup_mountafter.sh

--- setup_mountafter.sh
+++ setup_mountafter.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-env-update
-source /etc/profile
-export PS1="(chroot) $PS1"
 eselect profile set 4
 sed -i 's/#ja_JP.EUC-JP EUC-JP/ja_JP.EUC-JP EUC-JP/' /etc/locale.gen
 sed -i 's/#ja_JP.UTF-8 UTF-8/ja_JP.UTF-8 UTF-8/' /etc/locale.gen
@@ -10,4 +7,8 @@ sed -i 's/#ja_JP EUC-JP/ja_JP EUC-JP/' /etc/locale.gen
 sed -i 's/#en_US ISO-8859-1/en_US ISO-8859-1/' /etc/locale.gen
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-
+echo '
+env-update
+source /etc/profile
+export PS1="(chroot) $PS1"
+'
