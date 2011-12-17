7bdc54b1cbc2415e80350c20b4200171c843dba1
ngsw <ngsw@ngsw.jp>
2011-12-18 01:58:55 +0900

    emerge pciutils add

---
+++ emerge0.sh
@@ -0,0 +1,19 @@
+#1/bin/sh
+emerge vim
+emerge pciutils
+emerge gentoo-sources
+
+echo "LSMOD"
+read aaa
+lsmod
+read aaa
+
+echo "LSPCI"
+read aaa
+lspci
+read aaa
+
+cd /usr/src/linux
+ls -l
+
+make menuconfig
