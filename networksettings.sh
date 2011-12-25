ae125400953300a577d2c3d4fef4104235346cce
ngsw <ngsw@ngsw.jp>
2011-12-26 03:10:12 +0900

    modified:   networksettings.sh

--- networksettings.sh
+++ networksettings.sh
@@ -7,4 +7,7 @@ if [ ! "${_res}" -eq "0" ];then
 config_eth0="192.168.0.222 netmask 255.255.255.0 brd 192.168.0.255"
 routes_eth0="default via 192.168.0.1"
 NET.eof
+  cd /etc/init.d
+  ln -s net.lo net.eth0
+  rc-update add net.eth0 default
 fi
